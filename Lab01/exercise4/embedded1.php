0ef606cec3aa85ef50ee51d1e07ce38928c30406
quanta123645 <pdcoc123@gmail.com>
2022-04-07 15:36:23 +0700

    Add lab00 and lab01

---
+++ Lab01/exercise4/embedded1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HTML with PHP Embedded</title>
+    </head>
+    <body>
+        <font size=5 color="blue">Welcome to my page</font>
+        <?php
+            print ("<br> Using PHP is not hard<br>");
+        ?>
+        and you can learn to use it quickly!
+    </body>
+</html>
