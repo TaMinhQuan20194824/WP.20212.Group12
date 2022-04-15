bfca9ad2e409ebf454de3da6729f6aa2f72a870a
quanta123645 <pdcoc123@gmail.com>
2022-04-15 08:40:47 +0700

    Refactor 2.7 task

--- Lab02/Form4Radio2.php
+++ Lab02/Form4Radio2.php
@@ -5,8 +5,8 @@
     <body>
         <font size="5"> Thank You: Got Your Input.</font>
         <?php
-        $email = $_GET['email'];
-        $contact = $_GET['contact'];
+        $email = $_GET["email"];
+        $contact = $_GET["contact"];
         print("<br>Your email address is $email");
         print("<br>Contact preference is $contact");
         ?>
