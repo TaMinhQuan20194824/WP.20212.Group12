61b48acccb8bd50fe71becae2018f3bea96b957c
quanta123645 <pdcoc123@gmail.com>
2022-07-08 01:26:03 +0700

    Add Lab12

---
+++ Lab12/tests/Fixture/UsersFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'created' => '2022-07-07 18:00:05',
+                'modified' => '2022-07-07 18:00:05',
+            ],
+        ];
+        parent::init();
+    }
+}
